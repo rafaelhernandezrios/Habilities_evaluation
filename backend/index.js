86ae9e55565bf84241e11805ba319339ff024414
edgarhernandez94 <edgar_hdez94@hotmail.com>
2025-01-31 17:18:46 +0900

    Register

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoutes from "./routes/authRoutes.js";
+import { authRoutes } from "./routes/authRoutes.js"; 
 import surveyRoutes from "./routes/surveyRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
